1ebf9d92f4f508a3766e55ff4df6ac9645378cfc
nurmuhammad <nurmuhammadeshonov97@gmail.com>
2024-03-28 23:47:46 +0500

    added weeks calculator

---
+++ index.js
@@ -0,0 +1,17 @@
+let number = parseInt(prompt("sonni kiriting va hafta kunlarini bilib oling"));
+
+if (number == 1) {
+  console.log("dushanba");
+} else if (number == 2) {
+  console.log("seshanba");
+} else if (number == 3) {
+  console.log("chorshanba");
+} else if (number == 4) {
+  console.log("payshanba");
+} else if (number == 5) {
+  console.log("juma");
+} else if (number == 6) {
+  console.log("shanba");
+} else if (number == 7) {
+  console.log("yakshanba");
+}
